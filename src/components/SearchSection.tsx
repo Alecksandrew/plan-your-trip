9bc9ad50811f2adc76c144088f3a3de20feb2932
Alecksandrew <alecksandrewcontato@gmail.com>
2025-07-09 17:23:06 -0300

    SearchSection UI done!

--- src/components/SearchSection.tsx
+++ src/components/SearchSection.tsx
@@ -5,7 +5,7 @@ export default function SearchSection() {
     <div className="flex ">
       <label>
         <span className="text-base font-bold">Para onde você quer viajar?</span>
-        <div className="flex items-center border-2">
+        <div className="flex items-center border-2 container gap-2">
           <FaSearch />
           <input type="search" id="search" name="search" className="flex-1" />
         </div>
